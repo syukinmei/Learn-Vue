49db03400396d22a03f2deed0a50a250101bc991
syukinmei <347086009@qq.com>
2022-05-15 23:01:24 +0800

    5.15

--- 进阶/Vue3&Vite/src/main.js
+++ 进阶/Vue3&Vite/src/main.js
@@ -1,4 +1,9 @@
+// 引入的不再是Vue构造函数了，引入的是一个名为 createApp 的工厂函数
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 创建应用实例对象——app（类似于vue2中的vm，但app比vm更轻）
+// createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
+console.log(app);
